61bcb291f49cd87ecd99d06df42206d6b7bab94a
xochy <xochydeather@gmail.com>
2019-11-12 19:07:26 -0600

    Se agregan los archivos para la verificacion de operadores artmeticos y de comparacion

---
+++ libro/cap02/operadores/Aritmenticos.rb
@@ -0,0 +1,13 @@
+#Exponente
+puts 10 ** 2
+
+#Suma y resta
+puts 1 + 3
+puts 5 - 1
+
+#Multiplicacion y division
+puts 5 * 5
+puts 10 / 2
+
+#Modulo, devuelve el resto de la division
+puts 9 % 2
---
+++ libro/cap02/operadores/Comparacion.rb
@@ -0,0 +1,35 @@
+#Igualdad
+puts "Son iguales" if 1 == 1
+
+#Desigualdad
+puts "Son desiguales" if 1 != 2
+
+#Mayor que
+puts "Dos es mayor que Uno" if 2 > 1
+
+#Mayor o igual que
+puts "Dos es mayor o igual que Dos" if 2 >= 2
+
+#Menor que
+puts "Uno es menor que Dos" if 1 < 2
+
+#Menor o igual que
+puts "Dos es menor o igual que Dos" if 2 <= 2
+
+#Comprueba que el valor se encuentre dentro del rango especificado
+puts "15 se encuentra dentro del rango de 10..20" if (10..20) === 15
+
+#Metodo de comparacion de numeros
+a = 10
+b = 10.0
+c = 10
+
+puts a.eql?b
+puts a.eql?c
+
+#Devuelve: 1, si a > b; 2, si a = b; -1, si a < b
+
+a = 5
+b = 9
+
+puts a <=> b
